c3867e412752935c739c7746b420313e3d786c26
1989JJ1989 <justus.jobski@hotmail.de>
2022-05-05 22:18:03 +0200

    Function written to merge two csv files from Coinbase Pro and Basic into another master csv

--- Consolidate.py
+++ Consolidate.py
@@ -1,39 +1,51 @@
 import csv
 import os
+from datetime import datetime
 
+# Defining the function to merge two csv files from Coinbase Basic and Coinbase Pro
+def convert_coinbase_basic(tobeconverted1, tobeconverted2):
 
-def convert_coinbase_basic(tobeconverted, master):
-    path_tobeconverted = os.path.join(os.getcwd(), "fills_raw", tobeconverted)
-    path_master = os.path.join(os.getcwd(), "fills_raw", master)
+    # Setting the different paths to the files and to the merged file
+    path_tobeconverted1 = os.path.join(os.getcwd(), "fills_raw", tobeconverted1)
+    path_tobeconverted2 = os.path.join(os.getcwd(), "fills_raw", tobeconverted2)
+    path_merged = os.path.join(os.getcwd(), "merged_fills.csv")
 
+    # Mapping the different keys in the files to be merged to align the data
     key_mapping = {
-        "portfolio": "NEW",
-        "trade_id": "NEW",
-        "product": "NEW",
-        "side": "Type",
+        "portfolio": "Not Provided",
+        "trade id": "Not Provided",
+        "product": "Not Provided",
+        "side": "Transaction Type",
         "created at": "Timestamp",
         "size": "Quantity Transacted",
         "size unit": "Asset",
         "price": "Spot Price at Transaction",
-        "fee": "NEW",
-        "total": "Total",
+        "fee": "Fees",
+        "total": "Total (inclusive of fees)",
         "price/fee/total unit": "Spot Price Currency",
     }
 
-    with open(path_tobeconverted, "r") as csv_tobeconverted:
-        csv_reader = csv.DictReader(csv_tobeconverted)
-        print(csv_tobeconverted.closed)
+    # Opening the files to be merged
+    with open(path_tobeconverted1, "r") as csv_tbc1:
+        csv_reader1 = csv.DictReader(csv_tbc1)
 
-        # First off, apending to the end of the file
-        with open(path_master, "a") as csv_master:
-            csv_writer = csv.DictWriter(csv_master, fieldnames=key_mapping.keys())
-            print(csv_master.closed)
+        with open(path_tobeconverted2, "r") as csv_tbc2:
+            csv_reader2 = csv.DictReader(csv_tbc2)
 
-            for row in csv_reader:
-                trade = {}
-                for key, item in key_mapping.items():
-                    trade[key] = item
-                csv_writer.writerow(trade)
+            # Writing to the new file showing the merged data
+            with open(path_merged, "w") as csv_merged:
+                csv_writer = csv.DictWriter(csv_merged, fieldnames=key_mapping.keys())
 
+                csv_writer.writeheader()
 
-convert_coinbase_basic("fills_c.csv", "fills_cpro.csv")
+                for row in csv_reader1:
+                    trade = {}
+                    for key, item in key_mapping.items():
+                        trade[key] = row.get(item, "Not Provided")
+                    csv_writer.writerow(trade)
+
+                for row in csv_reader2:
+                    trade = {}
+                    for key, item in key_mapping.items():
+                        trade[key] = row[key]
+                    csv_writer.writerow(trade)
